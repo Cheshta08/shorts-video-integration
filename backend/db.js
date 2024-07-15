f20e42b1672123fb6d384a86bec4ee73d9f7bd68
Cheshta08 <111150320+Cheshta08@users.noreply.github.com>
2024-07-15 23:01:41 +0530

    Add files via upload

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+const connection_url = "mongodb+srv://carorabe21:diGNKxyoliPj1etg@cluster0.02503af.mongodb.net/videos";
 
+module.exports.connect = () => {
+  mongoose
+    .connect(connection_url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("connect successfully"))
+    .catch((err) => console.log("Error:", err));
+};
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
